811fc13498733ab2e88754d720a74d9233ff5e30
nagababu <nag4c1@gmail.com>
2023-01-10 14:24:31 +0530

    updating ./roboshoplatest-shell

--- frontend.sh
+++ frontend.sh
@@ -1,3 +1,5 @@
+source common.sh
+
 yum install nginx -y
 
 systemctl enable nginx
@@ -11,3 +13,7 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 cd /usr/share/nginx/html
 
 unzip /tmp/frontend.zip
+
+cp "$script_location"/files/roboshoplatest-shell/reverse_proxy.conf /etc/nginx/default.d/roboshop.conf
+
+systemctl restart nginx
