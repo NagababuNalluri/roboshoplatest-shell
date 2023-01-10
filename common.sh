98da0a871955eb2c4a3df4fafa174251f5271970
nagababu <nag4c1@gmail.com>
2023-01-10 17:42:56 +0530

    updating ./roboshoplatest-shell

--- common.sh
+++ common.sh
@@ -1,11 +1,14 @@
 script_location=$(pwd)
 log=/tmp/roboshop.log
 
+print_head() {
+  echo -e '\e[1m; $1 \e[0m'
+}
 status_check() {
   if [ $? -eq 0 ]
 then
-  echo '\e[31m success \e[0m'
+  echo -e '\e[31m success \e[0m'
   else
-    echo '\e[33m fail \e[0m'
+    echo -e '\e[33m fail \e[0m'
     fi
     }
