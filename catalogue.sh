4ba593500b5ef5c05a1adb32d97b75f4c54141c2
nagababu <nag4c1@gmail.com>
2023-01-10 16:34:28 +0530

    updating ./roboshoplatest-shell

--- catalogue.sh
+++ catalogue.sh
@@ -1,8 +1,10 @@
 source common.sh
 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+status_check
 
 yum install nodejs -y
+status_check
 
 id roboshop
 if [ $? -ne 0 ]
@@ -11,29 +13,31 @@ then
   fi
 
 mkdir -p /app
+status_check
 
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-
+status_check
 rm -rf /app/*
-
+status_check
 cd /app
-
+status_check
 unzip /tmp/catalogue.zip
-
+status_check
 cd /app
-
+status_check
 npm install
-
+status_check
 cp $script_location/files/catalogue.service /etc/systemd/system/catalogue.service
-
+status_check
 systemctl daemon-reload
-
+status_check
 systemctl enable catalogue
-
+status_check
 systemctl start catalogue
-
+status_check
 cp $script_location/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
-
+status_check
 yum install mongodb-org-shell -y
-
+status_check
 mongo --host mongodb-dev.devopshemasri.online </app/schema/catalogue.js
+status_check
--- common.sh
+++ common.sh
@@ -1,2 +1,11 @@
 script_location=$(pwd)
 log=/tmp/roboshop.log
+
+status_check() {
+  if [ $? -eq 0 ]
+then
+  echo 'success'
+  else
+    echo 'fail'
+    fi
+    }
